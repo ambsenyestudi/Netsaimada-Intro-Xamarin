c2c18d009561410b50a543f53d0655a2b1948f91
Adrià Freixas <adrifre7@hotmail.com>
2018-01-21 14:26:46 +0100

    MVVM Binding

---
+++ XamIntro/XamIntro/Views/EditExpenseView.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using XamIntro.ViewModels;
+
+namespace XamIntro.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class EditExpenseView : ContentPage
+	{
+		public EditExpenseView ()
+		{
+			InitializeComponent ();
+            this.BindingContext = new EditExpenseDetailViewModel();
+        }
+	}
+}
