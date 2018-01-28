3c29ec1f1e653eaa4b01a8134735007675be3c57
Adrià Freixas <adrifre7@hotmail.com>
2018-01-28 21:31:25 +0100

    Included in project

---
+++ XamIntro/XamIntro/ViewModels/ExpenseSummaryViewModel.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using XamIntro.Models;
+using XamIntro.Services;
+using XamIntro.Services.Contract;
+
+namespace XamIntro.ViewModels
+{
+    public class ExpenseSummaryViewModel:NavigationViewModel
+    {
+        private IDataService _dataService;
+        protected IDataService dataService {
+            get
+            {
+                if (_dataService == null)
+                {
+                    _dataService = App.Current.Resources["DataService"] as IDataService;
+                }
+                return _dataService;
+            }
+        }
+        private bool _isBusy;
+
+        public bool IsBusy
+        {
+            get { return _isBusy; }
+            set
+            {
+                _isBusy = value;
+                OnPropertyChanged("IsBusy");
+            }
+        }
+
+        public ObservableCollection<ExpenseModel> Expenses { get; set; }
+        public Command GetExpensesCommand { get; set; }
+
+        private ExpenseModel _selectedExpense;
+
+        public ExpenseModel SelectedExpense
+        {
+            get { return _selectedExpense; }
+            set
+            {
+                _selectedExpense = value;
+                OnPropertyChanged("SelectedExpense");
+                NavigationService.NavigateTo("EditExpenseView");
+            }
+        }
+
+
+        public ExpenseSummaryViewModel()
+        {
+            initializeViewModel();
+            //On Show workarround
+            MessagingCenter.Subscribe<NavigationService>(this, "Page pushed",(obj)=> GetExpensesCommand.Execute(null));
+        }
+
+        private async Task GetExpensesAync()
+        {
+            if(!IsBusy)
+            {
+                IsBusy = true;
+                try
+                {
+                    Expenses.Clear();
+                    var expenses = await dataService.GetExpensesAsync();
+                    foreach(var expense in expenses)
+                    {
+                        Expenses.Add(expense);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    string message = ex.Message;
+                }
+                finally
+                {
+                    IsBusy = false;
+                }
+            }
+        }
+        private void initializeViewModel()
+        {
+            Expenses = new ObservableCollection<ExpenseModel>();
+            GetExpensesCommand = new Command(async () => await GetExpensesAync());
+            //AddExpenseCommand = new Command(async () => await GetAddExpense());
+        }
+    }
+}
